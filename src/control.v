313a626a6d0f7220d8fe3c61978ed9b0367c0a49
dennisduu <157513761+dennisduu@users.noreply.github.com>
2025-02-15 18:00:27 -0500

    Create control.v

---
+++ src/control.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/11/2025 11:22:00 PM
+// Design Name: 
+// Module Name: control_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control_unit(
+
+
+    );
+endmodule
