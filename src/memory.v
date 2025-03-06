03b4908382583e866572fd426491216a0e761e10
RickGao <rickgao6@outlook.com>
2025-03-06 14:24:02 -0500

    Update memory.v

--- src/memory.v
+++ src/memory.v
@@ -35,15 +35,14 @@ module memory (
         end
     end
 
-    // Asynchronous read
+    // Asynchronous read using generate loop
     // Assign outputs based on read_enable and read_elem values
-    assign data_out[`DATA_WIDTH-1:0] = 
-        read_enable[0] ? mem[0][read_elem[1:0]] : {`DATA_WIDTH{1'b0}};
-    assign data_out[`DATA_WIDTH*2-1:`DATA_WIDTH] = 
-        read_enable[1] ? mem[1][read_elem[3:2]] : {`DATA_WIDTH{1'b0}};
-    assign data_out[`DATA_WIDTH*3-1:`DATA_WIDTH*2] = 
-        read_enable[2] ? mem[2][read_elem[5:4]] : {`DATA_WIDTH{1'b0}};
-    assign data_out[`DATA_WIDTH*4-1:`DATA_WIDTH*3] = 
-        read_enable[3] ? mem[3][read_elem[7:6]] : {`DATA_WIDTH{1'b0}};
+    genvar i;
+    generate
+        for (i = 0; i < 4; i = i + 1) begin : read_output_gen
+            assign data_out[`DATA_WIDTH*(i+1)-1:`DATA_WIDTH*i] = 
+                read_enable[i] ? mem[i][read_elem[i*2+1:i*2]] : {`DATA_WIDTH{1'b0}};
+        end
+    endgenerate
 
 endmodule
